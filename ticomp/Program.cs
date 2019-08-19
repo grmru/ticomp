32d0481dbaa31a78541819b65e1fa3a81c0724f1
Георгий Цырков <george@matias.ru>
2019-08-19 20:16:26 +0300

    Корректировка опций связанных с кодировкой

--- ticomp/Program.cs
+++ ticomp/Program.cs
@@ -9,24 +9,125 @@ namespace ticomp
         {
             System.IO.FileInfo fi = null;
 
+            System.Text.Encoding input_enc = System.Text.Encoding.Default;
+            System.Text.Encoding output_enc = System.Text.Encoding.Default;
+
+            System.IO.FileInfo fo = null;
+
+            bool verbous = false;
+
+            //for (int i = 0; i < args.Length; i++)
+            //{
+            //    Console.WriteLine(string.Format("[INFO]: args[{0}] = {1};", i, args[i]));
+            //}
+
             for (int i = 0; i < args.Length; i++)
-            {
+            { 
                 if (!args[i].StartsWith("-") &&
                     !args[i].StartsWith("--") &&
                     !args[i].StartsWith("\\"))
                 {
-                    fi = new System.IO.FileInfo(args[0]);
+                    fi = new System.IO.FileInfo(args[i]);
+                }
+                else
+                {
+                    string key = args[i].TrimStart('-').TrimStart('\\');
+                    if (key.StartsWith("ie"))
+                    {
+                        if (key.Contains("="))
+                        {
+                            string[] split = key.Split('=');
+                            try
+                            {
+                                int codepage = System.Text.Encoding.Default.CodePage;
+                                int.TryParse(split[1], out codepage);
+                                input_enc = System.Text.Encoding.GetEncoding(codepage);
+                            }
+                            catch (Exception ex)
+                            {
+                                Console.WriteLine("[ERROR]: " + ex.Message);
+                            }
+                        }
+                    }
+                    if (key.StartsWith("oe"))
+                    {
+                        if (key.Contains("="))
+                        {
+                            string[] split = key.Split('=');
+                            try
+                            {
+                                int codepage = System.Text.Encoding.Default.CodePage;
+                                int.TryParse(split[1], out codepage);
+                                output_enc = System.Text.Encoding.GetEncoding(codepage);
+                            }
+                            catch (Exception ex)
+                            {
+                                Console.WriteLine("[ERROR]: " + ex.Message);
+                            }
+                        }
+                    }
+                    if (key.StartsWith("out"))
+                    {
+                        if (key.Contains("="))
+                        {
+                            string[] split = key.Split('=');
+                            try
+                            {
+                                fo = new System.IO.FileInfo(split[1].Trim());
+                            }
+                            catch (Exception ex)
+                            {
+                                Console.WriteLine("[ERROR]: " + ex.Message);
+                            }
+                        }
+                    }
+                    if (key.StartsWith("verb"))
+                    {
+                        verbous = true;                        
+                    }
+
                 }
             }
 
             if (fi != null && fi.Exists)
             {
-                List<string> compiled_lines = CompileDocument(fi.FullName);
+                Console.OutputEncoding = output_enc;
 
-                for (int i = 0; i < compiled_lines.Count; i++)
+                if (verbous)
                 {
-                    Console.WriteLine(compiled_lines[i]);
+                    for (int i = 0; i < args.Length; i++)
+                    {
+                        Console.WriteLine(string.Format("[INFO]: args[{0}] = {1};", i, args[i]));
+                    }
+                    Console.WriteLine("[INFO]: Default codepage = " + System.Text.Encoding.Default.CodePage);
+                    Console.WriteLine("[INFO]: Input codepage = " + input_enc.CodePage);
+                    Console.WriteLine("[INFO]: Output codepage = " + output_enc.CodePage);
+                    Console.WriteLine("[INFO]: Console input codepage = " + Console.InputEncoding.CodePage);
+                    Console.WriteLine("[INFO]: Console output codepage = " + Console.OutputEncoding.CodePage);
+                    Console.WriteLine("[INFO]: Input filename = " + fi.FullName);
+                    if (fo != null)
+                    {
+                        Console.WriteLine("[INFO]: Output filename = " + fo.FullName);
+                    }
                 }
+
+                List<string> compiled_lines = CompileDocument(fi.FullName, input_enc);
+
+                if (fo != null)
+                {
+                    System.IO.File.WriteAllLines(fo.FullName, compiled_lines, output_enc);
+                }
+                else
+                {
+                    for (int i = 0; i < compiled_lines.Count; i++)
+                    {
+                        Console.WriteLine(compiled_lines[i]);
+                    }
+                }
+            }
+            else
+            {
+                Console.WriteLine("[ERROR]: не получилось считать файл " + fi != null ? fi.FullName : "null");
             }
 
 #if DEBUG
@@ -34,7 +135,7 @@ namespace ticomp
 #endif
         }
 
-        public static List<string> CompileDocument(string filePath)
+        public static List<string> CompileDocument(string filePath, System.Text.Encoding input_enc)
         {
             List<string> ret = new List<string>();
 
@@ -45,7 +146,7 @@ namespace ticomp
                 return ret;
             }
 
-            List<string> lines = new List<string>(System.IO.File.ReadAllLines(fi.FullName));
+            List<string> lines = new List<string>(System.IO.File.ReadAllLines(fi.FullName, input_enc));
 
             for (int l = 0; l < lines.Count; l++)
             {
@@ -67,7 +168,7 @@ namespace ticomp
                                 {
                                     string[] cmd = splitted_line[i].Split('=');
 
-                                    newLine = CompileDocument(cmd[1].Trim());
+                                    newLine = CompileDocument(cmd[1].Trim(), input_enc);
                                 }
 
                                 string result = string.Empty;
